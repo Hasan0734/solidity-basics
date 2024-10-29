5d5c9b5b0cbf152fa23d1947845946a2a36d77bf
Hasan0734 <Jahidhasan2856@gmail.com>
2024-10-29 08:29:21 +0600

    mapping , structs, enums, fixed array

--- DefaultValues.sol
+++ DefaultValues.sol
@@ -7,4 +7,5 @@ contract DefaultValues {
     int public i; // 0
     address public a; //  0x0000000000000000000000000000000000000000
     bytes32 public b32; // 0x0000000000000000000000000000000000000000000000000000000000000000
+    //mapping, structs, enums, fixed sized arrays
 }
